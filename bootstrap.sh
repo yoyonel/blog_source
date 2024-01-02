034d8d280bc933cd64b9bd4cb7372ace3d7b1697
ATTY Lionel <latty@unowhy.com>
2024-01-02 17:45:05 +0100

    feat: pip upgrade in bootstrap shell script
    Signed-off-by: ATTY Lionel <latty@unowhy.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,11 @@ PELICAN_PORT=${PELICAN_PORT:-8081}
 # python environment
 poetry install
 
+# [Semantic Versioning 2.0.0](https://semver.org/)
+# [notice] A new release of pip is available: major.minor.patch -> new_major.new_minor.new_patch
+# [notice] To update, run: pip install --upgrade pip
+pip install --upgrade pip
+
 # pelican themes & plugins
 ./add_extras_pelican-themes.sh
 ./add_extras_pelican-plugins.sh
